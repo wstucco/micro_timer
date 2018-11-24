8ee964e5235b532c62ffae0362a85f79fba100b5
Massimo Ronca <massimo@mikamai.com>
2018-11-25 00:08:34 +0100

    reduce CPU usage by using stdlib sleep when `timeout` is large enough

--- lib/micro_timer.ex
+++ lib/micro_timer.ex
@@ -21,7 +21,7 @@ defmodule MicroTimer do
 
   @spec usleep(non_neg_integer()) :: :ok
   def usleep(timeout) when is_integer(timeout) and timeout > 0 do
-    do_usleep(System.monotonic_time(:microsecond), timeout)
+    do_usleep(timeout)
 
     receive do
       @sleep_done -> :ok
@@ -33,6 +33,18 @@ defmodule MicroTimer do
   end
 
 
+  defp do_usleep(timeout) when timeout > 2_000 do
+    ms_timeout = div(timeout, 1_000) - 1
+    us_timeout = timeout - (ms_timeout * 1_000)
+
+    Process.sleep(ms_timeout)
+
+    do_usleep(System.monotonic_time(:microsecond), us_timeout)
+  end
+
+  defp do_usleep(timeout) do
+    do_usleep(System.monotonic_time(:microsecond), timeout)
+  end
 
   defp do_usleep(start, timeout) do
     if System.monotonic_time(:microsecond) - start >= timeout do
