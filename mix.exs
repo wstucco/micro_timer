3223cb94eb343fb85dc7846f45965935fb93fcf3
Massimo Ronca <massimo@mikamai.com>
2018-11-24 23:51:32 +0100

    add ex_doc to generate docs

--- mix.exs
+++ mix.exs
@@ -22,7 +22,9 @@ defmodule MicroTimer.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
   end
 
   defp package do
