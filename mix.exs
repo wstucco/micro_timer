c9593d77d18e33e2712d4e979c729b428ff32759
Massimo Ronca <massimo@mikamai.com>
2018-11-24 23:39:51 +0100

    add module configuration

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule MicroTimer.MixProject do
       app: :micro_timer,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: "A timer module with microsend resolution",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -20,9 +22,14 @@ defmodule MicroTimer.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      files: ~w(lib mix.exs README.md LICENSE),
+      maintainers: ["Massimo Ronca"],
+      licenses: ["MIT"]
     ]
   end
 end
